33722d7053026e4bff765ad24bdbbcc59521fe2d
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-04-27 18:28:14 -0400

    feat: home
     refactor: edit .env.example
    
     chore: install notion-to-md react-markdown and notionhq/client
    
     feat: notion implementation
    
     feat: notion implementation
    
     BREAKING CHANGE: Error: Error serializing  returned from  in /.
     Reason:  cannot be serialized as JSON. Please use  or omit this value.
    
     fix: async, then
    
     fix: use `jsx` extension over `js` for ReactJS files
    
     fix: missing `await` statements
    
     feat: blog card component
    
     feat: home
    
     fix(notion implementation): fix the cover
    
     feat: add a domain
    
     fix: image
    
     feat: implementation github actions

---
+++ src/pages/index.jsx
@@ -0,0 +1,31 @@
+import Layout from '../components/Layout';
+import { getPublishedBlogPosts } from '../lib/notion.js';
+import BlogCard from '../components/BlogCard';
+
+export async function getStaticProps() {
+  const posts = await getPublishedBlogPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default function Home({ posts }) {
+  return (
+    <Layout>
+      <h1 className="text-5xl">Last Post</h1>
+      <main className="max-w-5xl mx-auto relative">
+        <div className="h-full pt-4 pb-16 px-4 md:px-0 mx-auto">
+          <div className="flex items-center justify-center"></div>
+          <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-2 lg:max-w-none">
+            {posts.map((post) => (
+              <BlogCard key={post.id} post={post} />
+            ))}
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
