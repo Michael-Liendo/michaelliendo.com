2125cf6c87444432ab10c1d64cf13011df6c31e8
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-05-05 18:17:03 -0400

    feat(about me): about me with notion   chore: add about me database id
      feat: support for the about me
    
      feat: about me
    
      feat: workflow

--- src/pages/about-me.jsx
+++ src/pages/about-me.jsx
@@ -1,5 +1,27 @@
+import ReactMarkdown from 'react-markdown';
+
 import Layout from '../components/Layout';
 
-export default function AboutMe() {
-  return <Layout></Layout>;
+import { getAboutMe } from '../lib/notion';
+
+export async function getStaticProps() {
+  const aboutMe = await getAboutMe();
+
+  return {
+    props: {
+      markdown: aboutMe.markdown,
+    },
+  };
+}
+
+export default function AboutMe({ markdown }) {
+  return (
+    <Layout title="About Me | Michael Liendo">
+      <div className="flex items-center justify-center">
+        <article className="prose">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </div>
+    </Layout>
+  );
 }
