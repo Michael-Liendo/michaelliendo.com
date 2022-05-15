057ddec94c72cf104de615d514b74f1b8b7e8e04
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-05-15 17:07:27 -0400

    feat(#25): responsive
      feat: responsive header
    
      feat: responsive navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,15 @@ export default function Header() {
   return (
     <header className="h-30 bg-black">
       <div className="flex justify-between">
-        <h1 className="ml-16 mt-5 text-5xl font-mono font-black text-white">
+        <h1 className="ml-4 mt-5 text-3xl font-mono font-black text-white lg:ml-16 sm:text-5xl">
           Michael Liendo
         </h1>
         <div className="mr-16 mt-8">
           <Navbar />
         </div>
       </div>
-      <div className="flex justify-between mt-5">
-        <aside className="ml-16 mt-3.5 font-sans font-bold text-lg text-white">
+      <div className="lg:flex lg:justify-between lg:mt-5">
+        <aside className="block ml-4 my-3.5 font-sans font-bold text-xs text-white lg:ml-16 lg:text-lg">
           Frontend Developer 👨🏻‍💻 mostly working with Javascript and web
           technologies like React.
         </aside>
