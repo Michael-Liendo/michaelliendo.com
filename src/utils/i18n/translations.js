15421607b82b1f66ffb7def22df55d78c3739d71
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-08-09 10:30:47 -0400

    feat: navbar

--- src/utils/i18n/translations.js
+++ src/utils/i18n/translations.js
@@ -2,25 +2,17 @@ module.exports = {
   en: {
     title:
       'Michael Liendo - Front-end Developer. Learning, coding and teaching',
-    bio: 'Frontend Developer 👨🏻‍💻 mostly working with Javascript and web technologies like React.',
-    home: 'Home',
-    biography: 'Biography',
-    portfolio: 'Portfolio',
+    about: 'About',
+    knowledge: 'Knowledge',
+    work: 'Work',
     contact: 'Contact',
-    latest_blog_posts: 'Latest Posts',
-    description:
-      'Michael Liendo is a Frontend developer with a passion for learning, programming and teaching. He works in a team and is willing to learn whatever technology is needed to reach his goal.',
   },
   es: {
     title:
       'Michael Liendo - Front-end Developer. Aprendiendiendo, programando y enseñando',
-    bio: 'Desarrollador Frontend 👨🏻‍💻 trabajando principalmente con Javascript y tecnologías web como React.',
-    home: 'Inicio',
-    biography: 'Biografía',
-    portfolio: 'Portafolio',
+    about: 'Sobre Mi',
+    knowledge: 'Conocimiento',
+    work: 'Trabajo',
     contact: 'Contacto',
-    latest_blog_posts: 'Últimas publicaciones',
-    description:
-      'Michael Liendo es un desarrollador Frontend apasionado por aprender, programar y enseñar. Trabaja en equipo y está dispuesto a aprender cualquier tecnología que se necesite para llegar a su objetivo.',
   },
 };
