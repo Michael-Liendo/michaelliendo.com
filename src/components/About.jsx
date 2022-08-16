efcb3a6225b13aa514e8d300911e893a49a75e95
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-08-16 15:25:20 -0400

    feat: add translation to the about and best responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,11 @@
+import useTranslation from '../utils/i18n/hooks';
+
 export default function About() {
+  const t = useTranslation;
   return (
     <div className="" id="about">
-      <h2 className="text-6xl">About</h2>
-      <h3 className="w-1/2 mt-16">
-        My name is Michael Liendo and I am a web developer. I enjoy using my
-        skills and knowledge to create, improve, and complete projects, with the
-        goal of learning more and more. I became intrigued by this world at the
-        end of 2018 and from then on, I am what I am now, getting better and
-        better every day, to complete my goal of being at my peak.
-      </h3>
+      <h2 className="text-5xl md:text-6xl">{t('about')}</h2>
+      <h3 className="w-full mt-16 md:w-1/2">{t('biography')}</h3>
     </div>
   );
 }
--- src/utils/i18n/translations.js
+++ src/utils/i18n/translations.js
@@ -10,6 +10,8 @@ module.exports = {
     larger_bio:
       'I am a web developer. Using technologies like React, Next.js, most of the time I use Javascript.',
     curriculum: 'View Curriculum',
+    biography:
+      'My name is Michael Liendo and I am a web developer. I enjoy using my skills and knowledge to create, improve, and complete projects, with the goal of learning more and more. I became intrigued by this world at the end of 2018 and from then on, I am what I am now, getting better and better every day, to complete my goal of being at my peak.',
   },
   es: {
     title:
@@ -24,5 +26,7 @@ module.exports = {
     larger_bio:
       'Yo soy un desarrollador web. Usando tecnologías como React, Next.js, la mayor parte del tiempo uso Javascript.',
     curriculum: 'Ver Curriculum',
+    biography:
+      'Mi nombre es Michael Liendo y soy desarrollador web. Disfruto usar mis habilidades y conocimientos para crear, mejorar y completar proyectos, con el objetivo de aprender más y más. Me intrigó este mundo a fines de 2018 y, desde entonces, soy lo que soy ahora, cada día mejor y mejor, para completar mi objetivo de estar en mi peak.',
   },
 };
