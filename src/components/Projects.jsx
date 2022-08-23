06d45a4a8cbbf9a49b47e3e7bc30c88040a5857e
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-08-23 12:38:39 -0400

    fix: change the color and add animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,25 +6,34 @@ export default function Project({ projects }) {
   return (
     <div className="grid grid-cols-1 gap-10 lg:grid-cols-3 md:grid-cols-2 my-6 mt-28">
       {projects.map((project, index) => (
-        <div className="w-80 p-6 rounded-lg bg-slate-500" key={index}>
+        <div
+          className="w-80 p-6 rounded-lg bg-slate-600 bg-opacity-50"
+          key={index}
+        >
           <div className="flex justify-between">
             <Folder />
             <div className="flex">
-              <a className="mx-2" href={project.repository}>
+              <a
+                className="transition duration-300 hover:-translate-y-1 mx-2"
+                href={project.repository}
+              >
                 <GithubBorder />
               </a>
-              <a className="mx-2" href={project.preview}>
+              <a
+                className="transition duration-300 hover:-translate-y-1 mx-2"
+                href={project.preview}
+              >
                 <OpenLink />
               </a>
             </div>
           </div>
-          <div className="h-36">
+          <div className="h-44">
             <p className="my-3 text-xl">{project.title}</p>
-            <p>{project.description}</p>
+            <p className="text-indigo-200">{project.description}</p>
           </div>
           <div className="flex mt-3">
             {project.tags.map((tag) => (
-              <span className="text-sky-500 mx-1 font-medium" key={tag.id}>
+              <span className="text-sky-400 mx-1 font-medium" key={tag.id}>
                 {tag.name}
               </span>
             ))}
