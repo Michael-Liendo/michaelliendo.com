73c073bfeeb684d55177cded7208de843031acf8
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-08-23 17:58:19 -0400

    feat: changed to appear on all routes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,8 @@ import Head from 'next/head';
 
 import Navbar from './Navbar';
 import Footer from './Footer';
+import SocialMedia from '../components/SocialMedia';
+import Email from '../components/Email';
 
 import useTranslation from '../utils/i18n/hooks';
 
@@ -24,6 +26,8 @@ export default function Layout({ title, children }) {
 
       <Navbar />
       <main className="mx-10 min-h-screen md:mx-24">{children}</main>
+      <SocialMedia />
+      <Email />
       <Footer />
     </div>
   );
