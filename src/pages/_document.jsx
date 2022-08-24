e9aa6f0f8039fddd6288319d8f0106280bf8952b
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-08-24 11:44:56 -0400

    fix: some cookies

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -17,6 +17,7 @@ export default function MyDocument() {
             gtag('js', new Date());
             gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
               page_path: window.location.pathname,
+              cookie_flags: 'SameSite=None;Secure'
             });
           `,
           }}
