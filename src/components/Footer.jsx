cc9481cc3881087bd53f63e1a411dd13e5182148
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-08-24 12:18:08 -0400

    feat: headers order

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   let year = new Date().getFullYear();
   return (
     <div className="flex justify-between mx-10 my-8 md:mx-24">
-      <h5>
+      <h4>
         <a
           href="https://michaelliendo.com"
           className="transition text-gray-400 duration-300 hover:text-slate-50"
@@ -12,7 +12,7 @@ export default function Footer() {
           Michael Liendo
         </a>{' '}
         &copy; {year}
-      </h5>
+      </h4>
       <div className="hidden md:block">
         <Translate />
       </div>
