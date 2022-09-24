f79f0dc734e0b6e89b0ba324fa7aa583a32091a7
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-09-24 17:50:19 -0400

    feat: hero

--- src/utils/i18n/translations.js
+++ src/utils/i18n/translations.js
@@ -11,9 +11,8 @@ module.exports = {
     work: 'Work',
     contact: 'Contact',
     hi_my_name: 'Hi my name is',
-    short_bio: 'Frontend Developer mostly working with Javascript',
-    larger_bio:
-      'I am a web developer. Using technologies like React, Next.js, most of the time I use Javascript.',
+    short_bio:
+      'Frontend developer programming using Next.js to create solutions.',
     curriculum: 'View Curriculum',
     biography:
       'My name is Michael Liendo and I am a web developer. I enjoy using my skills and knowledge to create, improve, and complete projects, with the goal of learning more and more. I became intrigued by this world at the end of 2018 and from then on, I am what I am now, getting better and better every day, to complete my goal of being at my peak.',
@@ -40,9 +39,7 @@ module.exports = {
     contact: 'Contacto',
     hi_my_name: 'Hola, mi nombre es',
     short_bio:
-      'Desarrollador frontend que trabaja principalmente con Javascript',
-    larger_bio:
-      'Yo soy un desarrollador web. Usando tecnologías como React, Next.js, la mayor parte del tiempo uso Javascript.',
+      'Desarrollador frontend programando usando Next.js para crear soluciones.',
     curriculum: 'Ver Curriculum',
     biography:
       'Mi nombre es Michael Liendo y soy desarrollador web. Disfruto usar mis habilidades y conocimientos para crear, mejorar y completar proyectos, con el objetivo de aprender más y más. Me intrigó este mundo a fines de 2018 y, desde entonces, soy lo que soy ahora, cada día mejor y mejor, para completar mi objetivo de estar en mi peak.',
