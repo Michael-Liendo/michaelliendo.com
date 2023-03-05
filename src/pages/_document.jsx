9767b990ad4d9f85ead0ee363fb27db28c004142
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-05 14:17:18 -0400

    chore: remove google analytics

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,25 +4,6 @@ export default function MyDocument() {
   return (
     <Html className="scroll-smooth">
       <Head>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-              cookie_flags: 'SameSite=None;Secure'
-            });
-          `,
-          }}
-        />
-
         <link href="/favicon.ico" rel="shortcut icon" />
         <link
           href="/images/seo-images/apple-icon-180x180.png"
