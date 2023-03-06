a5d959fd1314c84c36d3ab5d13f96241858e3c78
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-06 12:05:54 -0400

    feat: social media

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,85 +1,37 @@
-import GithubBorder from './Icons/GithubBorder';
-import Linkedin from './Icons/Linkedin';
-import Twitter from './Icons/Twitter';
+import GithubIcon from '~icons/mdi/github.jsx';
+import TwitterIcon from '~icons/mdi/twitter.jsx';
+import Linkedin from '~icons/mdi/linkedin';
 
 export default function SocialMedia() {
   return (
-    <div>
-      <div className="mt-10 md:mt-0 md:flex md:flex-wrap md:flex-col md:items-center md:fixed md:bottom-0 md:right-4 md:opacity-100">
-        <div className="flex md:block mb-9">
-          <div className="mr-4 md:mb-5 transition duration-300 hover:-translate-y-1">
-            <a
-              title="Linkedin"
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.linkedin.com/in/michaelliendo/"
-              aria-label="Linkedin"
-            >
-              <Linkedin />
-            </a>
-          </div>
-          <div className="mr-4 md:mb-4 transition duration-300 hover:-translate-y-1">
-            <a
-              title="Twitter"
-              target="_blank"
-              rel="noreferrer"
-              href="https://twitter.com/MichaelMLiendo"
-              aria-label="Twitter"
-            >
-              <Twitter />
-            </a>
-          </div>
-          <div className="transition duration-300 hover:-translate-y-1">
-            <a
-              title="Github"
-              target="_blank"
-              rel="noreferrer"
-              href="https://github.com/Michael-Liendo"
-              aria-label="Github"
-            >
-              <GithubBorder />
-            </a>
-          </div>
-        </div>
-        <div className="hidden md:block bg-white w-0.5 h-56"></div>
-      </div>
-      <div className="hidden md:block">
-        <div className="flex">
-          <div className="mr-4 transition duration-300 hover:-translate-y-1">
-            <a
-              title="Linkedin"
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.linkedin.com/in/michaelliendo/"
-              aria-label="Linkedin"
-            >
-              <Linkedin />
-            </a>
-          </div>
-          <div className="mr-4 transition duration-300 hover:-translate-y-1">
-            <a
-              title="Twitter"
-              target="_blank"
-              rel="noreferrer"
-              href="https://twitter.com/MichaelMLiendo"
-              aria-label="Twitter"
-            >
-              <Twitter />
-            </a>
-          </div>
-          <div className="transition duration-300 hover:-translate-y-1">
-            <a
-              title="Github"
-              target="_blank"
-              rel="noreferrer"
-              href="https://github.com/Michael-Liendo"
-              aria-label="Github"
-            >
-              <GithubBorder />
-            </a>
-          </div>
-        </div>
-      </div>
+    <div className="flex space-x-1 sm:space-x-4">
+      <a
+        title="Linkedin"
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.linkedin.com/in/michaelliendo/"
+        aria-label="Linkedin"
+      >
+        <Linkedin style={{ fontSize: '1.5em', color: '#0A66C2' }} />
+      </a>
+      <a
+        title="Twitter"
+        target="_blank"
+        rel="noreferrer"
+        href="https://twitter.com/MichaelMLiendo"
+        aria-label="Twitter"
+      >
+        <TwitterIcon style={{ fontSize: '1.5em', color: '#1DA1F2' }} />
+      </a>
+      <a
+        title="Github"
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/Michael-Liendo"
+        aria-label="Github"
+      >
+        <GithubIcon style={{ fontSize: '1.5em', color: '' }} />
+      </a>
     </div>
   );
 }
