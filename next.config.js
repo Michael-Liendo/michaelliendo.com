a7c22a8c6eda8606e29fc9e06a57903a0b5283c1
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-06 11:50:33 -0400

    chore: install icons mdi

--- next.config.js
+++ next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
     locales: ['es', 'en'],
     defaultLocale: 'en',
   },
+  webpack(config) {
+    config.plugins.push(
+      require('unplugin-icons/webpack')({
+        compiler: 'jsx',
+        jsx: 'react',
+      }),
+    );
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
