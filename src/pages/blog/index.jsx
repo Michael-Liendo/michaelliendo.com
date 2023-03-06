fcb1fa6fefb92c9c3aab3d693d2e421d8340be9a
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-06 17:47:29 -0400

    feat: blog

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,3 +1,4 @@
+import Card from '../../components/Card';
 import Layout from '../../components/Layout';
 import PostCard from '../../components/PostCard';
 import { getPublishedBlogPosts } from '../../lib/notion.js';
@@ -18,16 +19,16 @@ export default function Home({ posts, locale }) {
       type="blog.website"
       keywords="michael liendo blog, blog of michael liendo, michaels posts, blog de michael"
     >
-      <h1 className="mt-20 mb-20 text-5xl text-center">
-        {t('latest_blog_posts')}
-      </h1>
-      <div className="md:grid grid-cols-3 gap-10">
-        {posts.map((post, index) => {
-          if (!(index % 4))
-            return <PostCard key={post.id} post={post} view={1} />;
-          else return <PostCard key={post.id} post={post} />;
-        })}
-      </div>
+      <Card className="p-7">
+        <h1 className="mb-20 text-5xl text-center">{t('latest_blog_posts')}</h1>
+        <div className="md:grid grid-cols-3 gap-10">
+          {posts.map((post, index) => {
+            if (!(index % 4))
+              return <PostCard key={post.id} post={post} view={1} />;
+            else return <PostCard key={post.id} post={post} />;
+          })}
+        </div>
+      </Card>
     </Layout>
   );
 }
