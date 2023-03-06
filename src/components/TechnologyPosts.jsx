8078ba633c960b65f811bd345a95889390f535dd
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-06 17:41:08 -0400

    feat: technologyPosts

---
+++ src/components/TechnologyPosts.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import Card from './Card';
+
+export default function TechnologyPosts({ posts }) {
+  return (
+    <Card className="p-4 w-full lg:w-1/4">
+      <h2 className="text-center text-3xl mb-6">Latest posts</h2>
+      <div className="">
+        {posts.slice(0, 7).map((post) => (
+          <Link href={`/blog/${post.url}`} key={post.id}>
+            <div
+              title={post.title}
+              className="flex transition ease-in hover:scale-105 duration-150 justify-between py-3 truncate"
+            >
+              <h3 className="truncate">{post.title}</h3>
+              <span>{post.date}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </Card>
+  );
+}
