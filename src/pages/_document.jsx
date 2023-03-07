e66ca603f0253e822a663feb36c746016fab26c3
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-07 16:44:18 -0400

    chore: remove icons

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,23 +5,6 @@ export default function MyDocument() {
     <Html className="scroll-smooth">
       <Head>
         <link href="/favicon.ico" rel="shortcut icon" />
-        <link
-          href="/images/seo-images/apple-icon-180x180.png"
-          rel="apple-touch-icon"
-          sizes="180x180"
-        />
-        <link
-          href="/images/seo-images/favicon-32x32.png"
-          rel="icon"
-          sizes="32x32"
-          type="image/png"
-        />
-        <link
-          href="/images/seo-images/favicon-16x16.png"
-          rel="icon"
-          sizes="16x16"
-          type="image/png"
-        />
       </Head>
       <body
         className="text-white"
