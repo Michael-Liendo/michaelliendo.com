8c9a0a8957da860c6eecc8f2412382ea40099cc0
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-15 15:18:09 -0400

    feat: document background color (#50)

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -7,7 +7,7 @@ export default function MyDocument() {
         <link href="/favicon.ico" rel="shortcut icon" />
       </Head>
       <body
-        className="text-white"
+        className="text-white bg-slate-800"
         style={{
           backgroundImage: `url("https://media4.giphy.com/media/TxVVB6PfWMjE4/giphy.gif?cid=ecf05e47bar9qnjddk6jmi3knyp0crh6nj4mnwcou98svv96&rid=giphy.gif&ct=g")`,
         }}
