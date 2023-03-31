b4b8c66dcc658a997b4f3f5bc1b5574af114347e
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-31 18:21:50 -0400

    chore: translates

--- src/utils/i18n/translations.js
+++ src/utils/i18n/translations.js
@@ -5,30 +5,16 @@ module.exports = {
     title: 'Michael Liendo | Frontend Developer',
     description:
       "Welcome to my web portfolio. I'm Michael Liendo, a Front-End developer specialized in React, Next.js, JavaScript and TypeScript. Explore my projects and contact me to discuss your next project.",
-    greetings: "Hi I'm Michael Liendo",
-    profile: 'Front-end Developer',
-    whoAmI: 'Who am I?',
-    biography:
-      'My name is Michael Liendo and I am a frontend programmer with over 2 years of experience in the field. My experience has given me a solid understanding of languages and technologies such as JavaScript, HTML, CSS, Next.js, React, Tailwind, Docker, Git and MongoDB. I am good at working in a team and pride myself on my problem solving and decision making skills. Also, I am always looking to learn and love to share my knowledge with others.',
-    availableWork: 'Available to work',
-
+    greetings: "Hi I'm Michael Liendo,",
+    aboutMe:
+      "I'm a Software Developer focused on React and TypeScript with a passion for Web Development. I enjoy working on challenging projects and value collaboration and problem-solving.",
     //navbar
     home: 'Home',
     projects: 'Projects',
-    workWithMe: 'Work with me',
+    notes: 'Notes',
 
-    // blog
-    latestBlogPosts: 'Latest Posts',
-
-    //work with me
-    workWithMeParagraph:
-      'If you are interested in working with me, you can contact me through my Twitter account or email. Just send me a direct message with a brief description of the project and the key details I need to know in order to start working on your project.',
-    workWithMeParagraph2:
-      'With all the details explained I can calculate a price estimate and a delivery date.',
-    workWithMeExample:
-      'Hello, I want a __________ software that allows me to do __________. This must be customizable, configurable and translatable. This is a list of features that the project needs to have:',
-    workWithMeExample2:
-      'This should be completed by __________ date. The project is inspired by _________ which you can use to get references.',
+    // notes
+    latestNotes: 'Latest Notes',
   },
   es: {
     locale: 'es',
@@ -36,29 +22,15 @@ module.exports = {
     title: 'Michael Liendo | Programador Front-end',
     description:
       'Bienvenido a mi portafolio web. Soy Michael Liendo, un desarrollador Front-End especializado en React, Next.js, JavaScript y TypeScript. Explora mis proyectos y contáctame para discutir tu próximo proyecto.',
-    greetings: 'Hola soy Michael Liendo',
-    profile: 'Programador Front-end | Programador de páginas webs',
-    whoAmI: '¿Quien soy yo?',
-    biography:
-      'Mi nombre es Michael Liendo y soy un programador frontend con más de 2 años de experiencia en el campo. Mi experiencia me ha dado una sólida comprensión de lenguajes y tecnologías como JavaScript, HTML, CSS, Next.js, React, Tailwind, Docker, Git y MongoDB. Soy bueno trabajando en equipo y me enorgullezco de mi capacidad para resolver problemas y tomar decisiones. Además, siempre estoy buscando aprender y me encanta compartir mis conocimientos con los demás.',
-    availableWork: 'Disponible para trabajar',
-
+    greetings: 'Hola soy Michael Liendo,',
+    aboutMe:
+      'Soy un Desarrollador de Software enfocado en React y TypeScript apasionado por el Desarrollo Web. Disfruto trabajar en proyectos desafiantes y valoro la colaboración y la resolución de problemas.',
     // Navbar
     home: 'Inicio',
     projects: 'Proyectos',
-    workWithMe: 'Trabajar conmigo',
+    notes: 'Notas',
 
-    // blog
-    latestBlogPosts: 'Últimos Posts',
-
-    //work with me
-    workWithMeParagraph:
-      'Si estás interesado en trabajar conmigo, puedes contactar conmigo a través de mi cuenta de Twitter o por correo electrónico. Envíame un mensaje directo con una breve descripción del proyecto y los detalles clave que necesito saber para empezar a trabajar en tu proyecto.',
-    workWithMeParagraph2:
-      'Con todos los detalles explicados puedo calcular una estimación de precio y una fecha de entrega.',
-    workWithMeExample:
-      'Hola, quiero un software __________ que me permita hacer __________. Este debe ser personalizable, configurable y traducible. Esta es una lista de características que el proyecto debe tener:',
-    workWithMeExample2:
-      'Deberá estar terminado para la fecha __________. El proyecto está inspirado en _________ que puede utilizar para obtener referencias.',
+    // notes
+    latestNotes: 'Últimas notas',
   },
 };
