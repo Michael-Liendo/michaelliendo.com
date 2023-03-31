dc02c372b5734905ce5d1042ab803394e9862870
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-31 16:23:55 -0400

    feat: social media

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,37 +1,46 @@
 import GithubIcon from '~icons/mdi/github.jsx';
 import TwitterIcon from '~icons/mdi/twitter.jsx';
-import Linkedin from '~icons/mdi/linkedin.jsx';
+import LinkedIn from '~icons/mdi/linkedin.jsx';
 
 export default function SocialMedia() {
   return (
-    <div className="flex space-x-1 sm:space-x-4">
-      <a
-        title="Linkedin"
-        target="_blank"
-        rel="noreferrer"
-        href="https://www.linkedin.com/in/michaelliendo/"
-        aria-label="Linkedin"
-      >
-        <Linkedin style={{ fontSize: '1.5em', color: '#0A66C2' }} />
-      </a>
-      <a
-        title="Twitter"
-        target="_blank"
-        rel="noreferrer"
-        href="https://twitter.com/MichaelMLiendo"
-        aria-label="Twitter"
-      >
-        <TwitterIcon style={{ fontSize: '1.5em', color: '#1DA1F2' }} />
-      </a>
-      <a
-        title="Github"
-        target="_blank"
-        rel="noreferrer"
-        href="https://github.com/Michael-Liendo"
-        aria-label="Github"
-      >
-        <GithubIcon style={{ fontSize: '1.5em' }} />
-      </a>
-    </div>
+    <>
+      <ul class="flex py-4 mx-auto md:mx-0">
+        <li class="mr-4">
+          <a
+            title="Github"
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Michael-Liendo"
+            aria-label="Github"
+          >
+            <GithubIcon style={{ fontSize: '1.5em' }} />
+          </a>
+        </li>
+        <li class="mr-4">
+          <a
+            title="LinkedIn"
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/michaelliendo/"
+            aria-label="LinkedIn"
+          >
+            <LinkedIn style={{ fontSize: '1.5em', color: '#0A66C2' }} />
+          </a>
+        </li>
+        <li class="mr-4">
+          <a
+            title="Twitter"
+            target="_blank"
+            rel="noreferrer"
+            href="https://twitter.com/MichaelMLiendo"
+            aria-label="Twitter"
+          >
+            <TwitterIcon style={{ fontSize: '1.5em', color: '#1DA1F2' }} />
+          </a>
+        </li>
+      </ul>
+      <div className="flex space-x-1 sm:space-x-4"></div>
+    </>
   );
 }
