912f51ccc3777105b650e335fc07071b1c925188
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-31 17:57:48 -0400

    style: config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          background: '#EBDFD1',
+          'background-alt': '#FDF1E2',
+        },
+        dark: {
+          background: '#0D0A12',
+          'background-alt': '#16101D',
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
