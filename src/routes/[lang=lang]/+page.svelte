ba08c5c179a872537555f019274da04b0d03bff9
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-14 10:14:47 -0400

    chore: add width a height attribute for icon images

--- src/routes/[lang=lang]/+page.svelte
+++ src/routes/[lang=lang]/+page.svelte
@@ -95,7 +95,12 @@
 						<div class="pl-2 flex items-center">
 							<figure class="w-[18px] h-[18px] self-center">
 								{#if note.icon?.external?.url}
-									<img src={note.icon?.external?.url} width="50" alt={note.icon?.external?.url} />
+									<img
+										src={note.icon?.external?.url}
+										width="40"
+										height="40"
+										alt={note.icon?.external?.url}
+									/>
 								{:else}
 									{note.icon?.emoji || ' '}
 								{/if}
