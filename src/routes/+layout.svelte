23ad5b4ea71939fd14083945d124a93c01f5a159
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-14 01:09:32 -0400

    chore: SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,21 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<meta name="theme-color" content="#3bd6cf" />
+
+	<meta name="author" content="Michael Liendo" />
+	<meta name="publisher" content="Michael Liendo" />
+	<meta name="copyright" content="Michael Liendo" />
+
+	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+</svelte:head>
+
 <div class="bg-light-background-alt dark:bg-dark-background-alt">
 	<div class="min-h-screen">
 		<Header />
-		<main class="max-w-4xl m-auto py-4"><slot /></main>
+		<main class="max-w-5xl m-auto p-4"><slot /></main>
 	</div>
 	<Footer />
 </div>
