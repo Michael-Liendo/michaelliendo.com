e1c9d9fa56836c529d31f2bf4428e9665ac6d0d3
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-14 01:08:36 -0400

    chore: change the types

--- src/routes/[lang=lang]/+page.ts
+++ src/routes/[lang=lang]/+page.ts
@@ -1,14 +1,16 @@
+import type { Note } from '$lib/utils/Notion/Notes/types.js';
+
 export const ssr = false;
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ params }) {
-	const res = await fetch(`api/notes/${params.lang}`);
+export async function load({ params, fetch }) {
+	const res = await fetch(`/api/notes/${params.lang}`);
 
 	if (res.ok) {
 		const notes = await res.json();
 
 		return {
-			notes
+			notes: notes as Note[]
 		};
 	}
 
--- src/routes/[lang=lang]/notes/[slug]/+page.ts
+++ src/routes/[lang=lang]/notes/[slug]/+page.ts
@@ -1,21 +1,23 @@
+import type { Note } from '$lib/utils/Notion/Notes/types.js';
+
 export const ssr = false;
 
 export async function load({ params, fetch }) {
 	try {
 		const request = await fetch(`/api/note/${params.lang}/${params.slug}`);
-		const post = await request.json();
+		const note = await request.json();
 
 		return {
-			note: post.note,
-			markdown: post.markdown
+			note: note.note as Note,
+			markdown: note.markdown as { parent: string }
 		};
 	} catch (error) {
 		const request = await fetch(`/api/note/${params.lang}/${params.slug}`);
 		const post = await request.json();
 
 		return {
-			note: post.note,
-			markdown: post.markdown
+			note: post.note as Note,
+			markdown: post.markdown as { parent: string }
 		};
 	}
 }
