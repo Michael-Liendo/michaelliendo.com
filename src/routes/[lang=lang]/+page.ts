e1273f1cb941feea9c734105dc69aa80781217b7
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-15 00:04:11 -0400

    fix: change to ssr (#57)
    * fix: change to ssr
    
    * fix: type fix ssr
    
    * chore: new version

--- src/routes/[lang=lang]/+page.ts
+++ src/routes/[lang=lang]/+page.ts
@@ -1,6 +1,6 @@
 import type { Note } from '$lib/utils/Notion/Notes/types.js';
 
-export const ssr = false;
+export const ssr = true;
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, fetch }) {
