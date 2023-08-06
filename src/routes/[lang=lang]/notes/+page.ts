aa19ca221e18b18c7c4f743b59f4c094b21ea124
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-08-06 12:25:55 -0400

    chore: format the code

--- src/routes/[lang=lang]/notes/+page.ts
+++ src/routes/[lang=lang]/notes/+page.ts
@@ -1,6 +1,5 @@
 import type { Note } from '$lib/utils/Notion/Notes/types.js';
 
-
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, fetch }) {
 	const res = await fetch(`/api/notes/${params.lang}`);
