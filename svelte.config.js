c8f9347dfd700b1aa9985d970e6dda0bc6d5682f
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-02-05 21:40:26 -0400

    chore: add path aliases for i18n in svelte.config and remove from tsconfig for better configuration management

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		alias:{
+			"$i18n": "./src/i18n",
+      "$i18n/*": "./src/i18n/*"
+		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
