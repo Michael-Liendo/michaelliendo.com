4b7841183d1b6351f641e7ef6c1973cc6fd19800
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-02-07 11:23:34 -0400

    feat: add Project type definition and refactor note loading logic for cleaner code and improved structure in Svelte application

---
+++ src/lib/types/Project.ts
@@ -0,0 +1,8 @@
+export interface Project {
+	name: string;
+	description: string;
+	url: string;
+	repo_url: string;
+	tags: string[];
+	preview_image_url?: string;
+}
--- src/routes/[[lang=lang]]/notes/[slug]/+page.ts
+++ src/routes/[[lang=lang]]/notes/[slug]/+page.ts
@@ -12,51 +12,38 @@ async function findAvailableLang(
 		try {
 			await import(`../../../../mdsvex/${slug}/${lang}.svx`);
 			return lang;
-		} catch (_) {
-			// biome-ignore lint/correctness/noUnnecessaryContinue: <explanation>
-			continue;
-		}
+		} catch (_) {}
 	}
 
 	return null;
 }
 
+async function loadPost(slug: string, lang: string) {
+	const post = await import(`../../../../mdsvex/${slug}/${lang}.svx`);
+	const content = post.default;
+	const metadata = post.metadata as NoteMetadata;
+
+	return {
+		note: {
+			...metadata,
+			content,
+		},
+	};
+}
+
 export async function load({ params }) {
 	const language = params.lang ?? baseLocale;
 
-	// FIXME: I dont like this at all, needs refactor but its 1:37 AM
 	try {
-		const post = await import(
-			`../../../../mdsvex/${params.slug}/${language}.svx`
-		);
-		const content = post.default;
-		const metadata = post.metadata as NoteMetadata;
-
-		return {
-			note: {
-				...metadata,
-				content,
-			},
-		};
-	} catch (error) {
-		// Failed to find the requested language, try to find a fallback language
+		return await loadPost(params.slug, language);
+	} catch (err) {
+		// Failed to load the requested language, will attempt fallback
 	}
 
 	const fallbackLang = await findAvailableLang(params.slug, language);
 
 	if (fallbackLang) {
-		const post = await import(
-			`../../../../mdsvex/${params.slug}/${fallbackLang}.svx`
-		);
-		const content = post.default;
-		const metadata = post.metadata as NoteMetadata;
-
-		return {
-			note: {
-				...metadata,
-				content,
-			},
-		};
+		return await loadPost(params.slug, fallbackLang);
 	}
 
 	throw error(404, "Not found");
